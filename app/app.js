eaa1bc032b8b26cd937eb3769989f94158dbd7cf
2swo <swlee456@naver.com>
2023-07-27 16:50:25 +0900

    refactor(#5) : CORS 설정 추가

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const movieRoutes = require("./src/routes/movieRoutes");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 // 미들웨어 등록 및 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // 라우터 연결
 app.use("/movie", movieRoutes);

b092b78f2d9e0982c5745d0795bd1044a42c5163
2swo <swlee456@naver.com>
2023-08-02 16:23:40 +0900

    refector(#5) : 라우트 경로 최적화

--- app/src/routes/movieRoutes.js
+++ app/src/routes/movieRoutes.js
@@ -7,15 +7,16 @@ const identifyToken = require("../middlewares/identifyToken");
 
 router.get("/lists", identifyToken.check.token, ctrl.output.getmovie); // 영화 목록
 router.get("/seats", identifyToken.check.token, ctrl.output.getSeat); // 좌석 조회
-router.get("/users/seat", identifyToken.check.token, ctrl.output.getUserSeat); // 예매된 유저 좌석 조회
+router.get("/myseats", identifyToken.check.token, ctrl.output.getUserSeat); // 예매된 유저 좌석 조회
 
 router.post("/seats", identifyToken.check.token, ctrl.intput.reserveSeat); // 좌석 예매
 
 router.delete("/users/seat", identifyToken.check.token, ctrl.intput.cancelSeat); // 예매 취소
 
-router.get("/movies/like", identifyToken.check.token, ctrl.output.getmovielike); //영화 좋아요 가져오기
+router.get("/like", identifyToken.check.token, ctrl.output.getmovielike); //영화 좋아요 가져오기
 router.patch(
-  "/movies/like/:movie_id",
+  // 로그인 id 가 토큰으로 발급되서 토큰에서 가져와야함.
+  "/like/:movie_id",
   identifyToken.check.token,
   ctrl.process.updatemovielike
 ); // 영화 좋아요 업데이트

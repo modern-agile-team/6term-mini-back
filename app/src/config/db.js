11cc827968f2b68bcd0460cae5ec3e2cc3cad333
2swo <swlee456@naver.com>
2023-08-02 13:36:51 +0900

    docs(#5) : mysql2 접속 끊김 해결

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
-require('dotenv').config(); // 환경 변수를 .env 파일에서 가져오기
+require("dotenv").config(); // 환경 변수를 .env 파일에서 가져오기
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-db.connect();
+db.js();
 
 module.exports = db;

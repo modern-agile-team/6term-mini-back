25438ea0ccb3f96c55c88de7213333900e3ceb54
NicoDora <pjh76574734@gmail.com>
2023-08-02 12:56:58 +0900

    style(#6): API URI수정

--- app/src/routes/movieRoutes.js
+++ app/src/routes/movieRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const ctrl = require("../controllers/movie.ctrl");
 const identifyToken = require("../middlewares/identifyToken");
 
-router.get("/movies", identifyToken.check.token, ctrl.output.getmovie); // 영화 목록
-router.get("/movies/seat", identifyToken.check.token, ctrl.output.getSeat); // 좌석 조회
-router.get("/movies/seat/user", identifyToken.check.token, ctrl.output.getUserSeat); // 예매된 유저 좌석 조회
+router.get("/lists", identifyToken.check.token, ctrl.output.getmovie); // 영화 목록
+router.get("/seats", identifyToken.check.token, ctrl.output.getSeat); // 좌석 조회
+router.get("/users/seat", identifyToken.check.token, ctrl.output.getUserSeat); // 예매된 유저 좌석 조회
 
-router.post("/movies/seat", identifyToken.check.token, ctrl.intput.reserveSeat); // 좌석 예매
+router.post("/seats", identifyToken.check.token, ctrl.intput.reserveSeat); // 좌석 예매
 
 module.exports = router;

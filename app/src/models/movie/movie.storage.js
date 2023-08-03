3d2f88d5548979dfeadb929c392d7d1e79d9268b
NicoDora <pjh76574734@gmail.com>
2023-08-03 20:49:29 +0900

    refactor(#2): 쿼리문 수정

--- app/src/models/movie/movie.storage.js
+++ app/src/models/movie/movie.storage.js
@@ -6,8 +6,8 @@ class movieStorage {
   static async getmovie() {
     try {
       const sql = `SELECT * FROM movie`;
-      const results = await db.query(sql);
-      return results[0];
+      const results = (await db.query(sql))[0];
+      return results;
     } catch (error) {
       console.log(error);
     }
